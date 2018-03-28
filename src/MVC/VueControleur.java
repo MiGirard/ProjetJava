f67dc1d3b6065fad3e2f534bb4c2aad11c387524
Mihajlo GIRARD <mihajlo.girard@hotmail.fr>
2018-03-28 16:33:30 +0200

    Grille de Canvas
    Grille de canvas

--- src/MVC/VueControleur.java
+++ src/MVC/VueControleur.java
@@ -19,6 +19,8 @@ import static javafx.application.Application.launch;
 
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
@@ -33,6 +35,17 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
 
 public class VueControleur extends Application {
 
@@ -45,19 +58,18 @@ public class VueControleur extends Application {
 
         // initialisation du modèle que l'on souhaite utiliser
         m = new Modele();
-
         // gestion du placement (permet de palcer le champ Text affichage en haut, et GridPane gPane au centre)
         BorderPane border = new BorderPane();
 
         // permet de placer les diffrents boutons dans une grille
         GridPane gPane = new GridPane();
 
-        Text[][] tabText = new Text[5][5];
-
         Text affichage = new Text("Grille Drag&Drop");
         affichage.setFont(Font.font("Verdana", 30));
         affichage.setFill(Color.RED);
         border.setTop(affichage);
+        
+
 
         // la vue observe les "update" du modèle, et réalise les mises à jour graphiques
         m.addObserver(new Observer() {
@@ -67,49 +79,12 @@ public class VueControleur extends Application {
                 // TODO
             }
         });
-
-        for (int column = 0; column < 5; column++) {
+        
+       for (int column = 0; column < 5; column++) {
             for (int row = 0; row < 5; row++) {
-
-                final int fColumn = column;
-                final int fRow = row;
-
-                final Text t = new Text(" " + column + "-" + row + " ");
-                tabText[column][row] = t;
-                t.setFont(Font.font("Verdana", 25));
-                
-                
-                t.setOnDragDetected(new EventHandler<MouseEvent>() {
-                    public void handle(MouseEvent event) {
-
-                        Dragboard db = t.startDragAndDrop(TransferMode.ANY);
-                        ClipboardContent content = new ClipboardContent();       
-                        content.putString(""); // non utilisé actuellement
-                        db.setContent(content);
-                        event.consume();
-                        m.startDD(fColumn, fRow);
-                    }
-                });
-
-                t.setOnDragEntered(new EventHandler<DragEvent>() {
-                    public void handle(DragEvent event) {
-                        
-                        m.parcoursDD(fColumn, fRow);
-                        event.consume();
-                    }
-                });
-                
-                t.setOnDragDone(new EventHandler<DragEvent>() {
-                    public void handle(DragEvent event) {
-                        
-                        // attention, le setOnDragDone est déclenché par la source du Drag&Drop
-                        
-                        m.stopDD(fColumn, fRow);
-                        
-                    }
-                });
-
-                gPane.add(tabText[column][row], column, row);
+                Canvas canvas = new Canvas(50,50);
+                initCanvas(canvas);
+                gPane.add(canvas, column, row);    
             }
         }
 
@@ -122,7 +97,67 @@ public class VueControleur extends Application {
         primaryStage.setTitle("Drag & Drop");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
     }
+    
+    
+        private void initDraw(GraphicsContext gc){
+        double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(5);
+
+        gc.fill();
+        gc.strokeRect(
+                0,              //x of the upper left corner
+                0,              //y of the upper left corner
+                canvasWidth,    //width of the rectangle
+                canvasHeight);  //height of the rectangle
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+
+    }
+        
+        private void initCanvas(Canvas canvas){
+            
+            final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+            initDraw(graphicsContext);
+            
+            canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+            new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                graphicsContext.beginPath();
+                graphicsContext.moveTo(event.getX(), event.getY());
+                graphicsContext.stroke();
+            }
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
+                new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                graphicsContext.lineTo(event.getX(), event.getY());
+                graphicsContext.stroke();
+            }
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
+                new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+
+            }
+        });
+        }
+
 
     /**
      * @param args the command line arguments
@@ -130,5 +165,9 @@ public class VueControleur extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
+    
+
+
 
 }
