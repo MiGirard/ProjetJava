a644fb97d3913fa047e861deb3a5fc3c647c41d5
Mihajlo GIRARD <mihajlo.girard@hotmail.fr>
2018-03-28 15:29:05 +0200

    VueControleur Code grille DD
    Création du fichier VueControleur avec le code de la classe VueControleur de la grille drag and drop

--- src/MVC/Modele.java
+++ src/MVC/Modele.java
@@ -3,12 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package MVC;
+
+import java.util.Observable;
 
 /**
  *
  * @author p1506391
  */
-public class Modele {
+public class Modele extends Observable {
+    int lastC, lastR;
+    
+    public void startDD(int c, int r) {
+        // TODO
+        System.out.println("startDD : " + c + "-" + r);
+        setChanged();
+        notifyObservers();
+    }
+    
+    public void stopDD(int c, int r) {
+        // TODO
+        
+        // mémoriser le dernier objet renvoyé par parcoursDD pour connaitre la case de relachement
+        
+        System.out.println("stopDD : " + c + "-" + r + " -> " + lastC + "-" + lastR);
+        setChanged();
+        notifyObservers();
+    }
+    
+    public void parcoursDD(int c, int r) {
+        // TODO
+        lastC = c;
+        lastR = r;
+        System.out.println("parcoursDD : " + c + "-" + r);
+        setChanged();
+        notifyObservers();
+    }
     
 }
